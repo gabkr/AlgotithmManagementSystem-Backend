f2ad02b862d7c3b78cbae2a3c3ba1812df13ea65
Abhishek Gotike <agotike@wpi.edu>
2021-12-16 14:31:16 -0500

    User HistorY

---
+++ AlgorithmManagementBackend/src/main/java/cs509/thalassa/demo/model/UserHistory.java
@@ -0,0 +1,33 @@
+package cs509.thalassa.demo.model;
+
+public class UserHistory {
+	public final String Username;
+	public final String UserID;
+	public final String Activity;
+	public final String ActivityTime;
+	
+	public UserHistory (String UserID, String Username, String Activity, String ActivityTime) {
+		this.UserID = UserID;
+		this.Username = Username;
+		this.Activity = Activity;
+		this.ActivityTime = ActivityTime;
+	}
+
+	/**
+	 * Equality of Constants determined by name alone.
+	 */
+	public boolean equals (Object o) {
+		if (o == null) { return false; }
+		
+		if (o instanceof UserHistory) {
+			UserHistory other = (UserHistory) o;
+			return Username.equals(other.Username);
+		}
+		
+		return false;  // not a userhistory
+	}
+
+	public String toString() {
+		return "[" + UserID + " " + Username + " " + Activity + ActivityTime + "]";
+	}
+}
