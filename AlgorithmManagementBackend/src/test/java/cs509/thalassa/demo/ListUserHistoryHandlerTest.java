8d5bc251bdd92fa237905738436a7727befddbda
Abhishek Gotike <agotike@wpi.edu>
2021-12-16 23:14:27 -0500

    List UserHistory handler added

---
+++ AlgorithmManagementBackend/src/test/java/cs509/thalassa/demo/ListUserHistoryHandlerTest.java
@@ -0,0 +1,75 @@
+package cs509.thalassa.demo;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+import cs509.thalassa.demo.http.UserHistoryRequest;
+import cs509.thalassa.demo.http.UserHistoryResponse;
+
+/**
+ * A simple test harness for locally invoking your Lambda function handler.
+ */
+public class ListUserHistoryHandlerTest extends LambdaTest {
+
+    void testSuccessInput(String incoming) throws IOException {
+    	ListUserHistoryHandler handler = new ListUserHistoryHandler();
+    	UserHistoryRequest req = new Gson().fromJson(incoming, UserHistoryRequest.class);
+       
+        UserHistoryResponse resp = handler.handleRequest(req, createContext("create"));
+        Assert.assertEquals(200, resp.statusCode);
+    }
+	
+    void testFailInput(String incoming, int failureCode) throws IOException {
+    	ListUserHistoryHandler handler = new ListUserHistoryHandler();
+    	UserHistoryRequest req = new Gson().fromJson(incoming, UserHistoryRequest.class);
+
+    	UserHistoryResponse resp = handler.handleRequest(req, createContext("create"));
+        Assert.assertEquals(failureCode, resp.statusCode);
+    }
+
+   
+    // NOTE: this proliferates large number of constants! Be mindful
+    @Test
+    public void testShouldBeOk() {
+    	int rndNum = (int)(990*(Math.random()));
+    	String var = "throwAway" + rndNum;
+    	
+    	UserHistoryRequest ccr = new UserHistoryRequest("UserID5");
+        String SAMPLE_INPUT_STRING = new Gson().toJson(ccr);  
+        
+        try {
+        	testSuccessInput(SAMPLE_INPUT_STRING);
+        } catch (IOException ioe) {
+        	Assert.fail("Invalid:" + ioe.getMessage());
+        }
+    }
+    
+    /**
+    @Test
+    public void testGarbageInput() {
+    	String SAMPLE_INPUT_STRING = "{\"sdsd\": \"e3\", \"hgfgdfgdfg\": \"this is not a number\"}";
+        
+        try {
+        	testFailInput(SAMPLE_INPUT_STRING, 400);
+        } catch (IOException ioe) {
+        	Assert.fail("Invalid:" + ioe.getMessage());
+        }
+    }
+    **/
+    /**
+    // overwrites into it
+    @Test
+    public void testCreateSystemConstant() {
+    	// create constant
+    	int rndNum = (int)(990*(Math.random()));
+        CreateConstantRequest csr = new CreateConstantRequest("to-delete-again" + rndNum, 9.29837, true);
+        
+        CreateConstantResponse resp = new CreateConstantHandler().handleRequest(csr, createContext("create"));
+        Assert.assertEquals(200, resp.httpCode);
+    }
+    **/
+}
